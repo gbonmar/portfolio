36f79ba3ef4e831f3fbe0fbe8ad5f0f4cdb2f42b
Bonmar Guilleno <128605539+iloveiap@users.noreply.github.com>
2023-06-15 01:29:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Random Quote Generator start
+var quotes = [
+  "Roses are red, violets are blue, tang ina mu - Bonmar Guilleno",
+  "Atleast di ako cheater, par. - John Harold Pineda",
+  "Saan tayo par? Teka lang mag sunod lang ako. - John Clark Pineda",
+  "Oo nga nickers, ambobo mo. -Julius Canares",
+  "Awit sayo par. -Nathaniel Bilo",
+  "Cobra tayo par. -Nikko Prudente",
+  "Tara nomi. -Jc Vasquez",
+];
+
+function generateQuote() {
+  var randomIndex = Math.floor(Math.random() * quotes.length);
+  var quoteElement = document.getElementById("quote");
+  quoteElement.innerHTML = quotes[randomIndex];
+}
+//end
+
+
 document.querySelectorAll('a[href^="#"').forEach(anchor => {
   anchor.addEventListener("click", function(e){
     e.preventDefault();
@@ -7,4 +26,10 @@ document.querySelectorAll('a[href^="#"').forEach(anchor => {
   })
 })
 
-
+ScrollReveal().reveal('.container', {
+  delay: 200,
+  distance: '50px',
+  easing: 'ease-in-out',
+  origin: 'bottom',
+  interval: 100
+});
