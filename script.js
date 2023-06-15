0d8f0ac9072c5029f36330ac881add324d5a6331
Bonmar Guilleno <128605539+iloveiap@users.noreply.github.com>
2023-06-15 00:53:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,5 @@ document.querySelectorAll('a[href^="#"').forEach(anchor => {
     });
   })
 })
-const firebaseConfig = {
-  apiKey: "AIzaSyA3hNSfCVyNqAk83UkasRGas4Vp9vSYlBU",
-  authDomain: "chat-app-portfolio-24bc8.firebaseapp.com",
-  databaseURL: "https://chat-app-portfolio-24bc8-default-rtdb.firebaseio.com",
-  projectId: "chat-app-portfolio-24bc8",
-  storageBucket: "chat-app-portfolio-24bc8.appspot.com",
-  messagingSenderId: "845272638719",
-  appId: "1:845272638719:web:04c2d34f43285acc65d8ca",
-  measurementId: "G-PM4GZLN564"
-};
 
 
