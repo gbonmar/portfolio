98c227fa366153f3bf4acc959bcbb008007a23a8
Bonmar Guilleno <128605539+iloveiap@users.noreply.github.com>
2023-06-15 00:04:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,10 @@ document.querySelectorAll('a[href^="#"').forEach(anchor => {
     });
   })
 })
+import firebase from './firebaseConfig';
+
+// Now you can use the `firebase` object to interact with Firebase services
+const database = firebase.database();
+// ...
 
 
-import firebase from 'firebaseConfig';
-import 'firebase/database';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA3hNSfCVyNqAk83UkasRGas4Vp9vSYlBU",
-  authDomain: "chat-app-portfolio-24bc8.firebaseapp.com",
-  databaseURL: "https://chat-app-portfolio-24bc8-default-rtdb.firebaseio.com",
-  projectId: "chat-app-portfolio-24bc8",
-  storageBucket: "chat-app-portfolio-24bc8.appspot.com",
-  messagingSenderId: "845272638719",
-  appId: "1:845272638719:web:04c2d34f43285acc65d8ca"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
