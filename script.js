5e49198a07f5e3052c2a48bc9ccabf511702a22f
Bonmar Guilleno <128605539+iloveiap@users.noreply.github.com>
2023-06-14 23:51:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ document.querySelectorAll('a[href^="#"').forEach(anchor => {
 })
 
 
-// Initialize Firebase
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 const firebaseConfig = {
   apiKey: "AIzaSyA3hNSfCVyNqAk83UkasRGas4Vp9vSYlBU",
   authDomain: "chat-app-portfolio-24bc8.firebaseapp.com",
@@ -19,50 +21,12 @@ const firebaseConfig = {
   appId: "1:845272638719:web:04c2d34f43285acc65d8ca"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Get the database reference
+export default firebase;
+
+import firebase from './firebaseConfig';
+
+// Now you can use the `firebase` object to interact with Firebase services
 const database = firebase.database();
-
-// Get chat elements
-const chatForm = document.getElementById('chat-form');
-const messageInput = document.getElementById('message-input');
-const chatMessages = document.getElementById('chat-messages');
-
-// Function to send a message
-function sendMessage(e) {
-  e.preventDefault();
-  const message = messageInput.value.trim();
-
-  if (message) {
-    // Push the message to the database
-    database.ref('messages').push().set({
-      text: message,
-      timestamp: firebase.database.ServerValue.TIMESTAMP
-    });
-
-    messageInput.value = '';
-  }
-}
-
-// Function to display the chat messages
-function displayMessages(snapshot) {
-  chatMessages.innerHTML = '';
-
-  snapshot.forEach(function(childSnapshot) {
-    const message = childSnapshot.val().text;
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    chatMessages.appendChild(messageElement);
-  });
-
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-}
-
-// Listen for new messages in the database
-database.ref('messages').on('value', function(snapshot) {
-  displayMessages(snapshot);
-});
-
-// Event listener for the chat form submission
-chatForm.addEventListener('submit', sendMessage);
